fe29bcf7c98210c7131ecb5279cb8bcc9830a019
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-01-03 08:21:58 +0100

    Now compiles with bari

--- src/dotnetplugins/Bari.Plugins.CodeContracts/cs/Properties/AssemblyInfo.cs
+++ src/dotnetplugins/Bari.Plugins.CodeContracts/cs/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("68ee4072-c2db-4f60-ad1c-a86d24440a03")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
