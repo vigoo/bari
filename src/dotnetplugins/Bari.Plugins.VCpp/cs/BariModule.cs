c9575fd3d936506313814338fc5281c093e05956
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-06-04 09:20:51 +0200

    C++ module bootstrap (issue #14)

---
+++ src/dotnetplugins/Bari.Plugins.VCpp/cs/BariModule.cs
@@ -0,0 +1,20 @@
+﻿using Ninject.Modules;
+
+namespace Bari.Plugins.VCpp
+{
+    /// <summary>
+    /// The module definition of this bari plugin
+    /// </summary>
+    public class BariModule: NinjectModule
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof (BariModule));
+
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            log.Info("Visual C++ plugin loaded");
+        }
+    }
+}
