c1a35e86ba1e5675b07ae4691d88db85fcef7c94
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-07-24 08:40:08 +0200

    Finished C++ project properties YAML loader (part of issue #14)

--- src/dotnetplugins/Bari.Plugins.VCpp/cs/Model/Loader/VCppCompilerParametersLoader.cs
+++ src/dotnetplugins/Bari.Plugins.VCpp/cs/Model/Loader/VCppCompilerParametersLoader.cs
@@ -71,13 +71,118 @@ namespace Bari.Plugins.VCpp.Model.Loader
                     {"browse-information-file", () => target.BrowseInformationFile = ParseString(value) },
                     {"buffer-security-check", () => target.BufferSecurityCheck = ParseBool(value) },
                     {"calling-convention", () => target.CallingConvention = ParseEnum<CallingConvention>(value, "calling convention") },
-                    {"compile-as", () => target.CompileAs = }
+                    {"compile-as", () => target.CompileAs = ParseCLanguage(value) },
+                    {"compile-as-managed", () => target.CompileAsManaged = ParseEnum<ManagedCppType>(value, "managed c++ type")},
+                    {"create-hotpatchable-image", () => target.CreateHotpatchableImage = ParseBool(value)},
+                    {"debug-information-format", () => target.DebugInformationFormat = ParseEnum<DebugInformationFormat>(value, "debug information format")},
+                    {"disable-language-extensions", () => target.DisableLanguageExtensions = ParseBool(value)},
+                    {"supressed-warnings", () => target.SuppressedWarnings = ParseWarnings(value)},
+                    {"enhanced-instruction-set", () => target.EnableEnhancedInstructionSet = ParseEnum<EnhancedInstructionSet>(value, "enhanced instruction set")},
+                    {"fiber-safe-optimizations", () => target.EnableFiberSafeOptimizations = ParseBool(value)},
+                    {"code-analysis", () => target.CodeAnalysis = ParseBool(value)},
+                    {"exception--handling", () => target.ExceptionHandling = ParseEnum<ExceptionHandlingType>(value, "exception handling type")},
+                    {"expand-attributed-source", () => target.ExpandAttributedSource = ParseBool(value)},
+                    {"favor", () => target.Favor = ParseEnum<OptimizationFavor>(value, "optimization favor")},
+                    {"floating-point-exceptions", () => target.FloatingPointExceptions = ParseBool(value)},
+                    {"floating-point-model", () => target.FloatingPointModel = ParseEnum<FloatingPointModel>(value, "floating point model")},
+                    {"force-conformance-in-for-loop-scope", () => target.ForceConformanceInForLoopScope = ParseBool(value)},
+                    {"forced-include-files", () => target.ForcedIncludeFiles = ParseStringArray(value)},
+                    {"forced-using-files", () => target.ForcedUsingFiles = ParseStringArray(value)},
+                    {"function-level-linking", () => target.FunctionLevelLinking = ParseBool(value)},
+                    {"generate-xml-documentation-files", () => target.GenerateXMLDocumentationFiles = ParseBool(value)},
+                    {"ignore-standard-include-path", () => target.IgnoreStandardIncludePath = ParseBool(value)},
+                    {"inline-function-expansion", () => target.InlineFunctionExpansion = ParseEnum<InlineExpansion>(value, "inline expansion")},                    
+                    {"intrinsic-functions", () => target.IntrinsicFunctions = ParseBool(value)},
+                    {"minimal-rebuild", () => target.MinimalRebuild = ParseBool(value)},
+                    {"multi-processor-compilation", () => target.MultiProcessorCompilation = ParseBool(value)},
+                    {"omit-default-lib-name", () => target.OmitDefaultLibName = ParseBool(value)},
+                    {"omit-frame-pointers", () => target.OmitFramePointers = ParseBool(value)},
+                    {"openmp-support", () => target.OpenMPSupport = ParseBool(value)},
+                    {"optimization", () => target.Optimization = ParseEnum<OptimizationLevel>(value, "optimization level")},
                     {"defines", () => target.Defines = ParseStringArray(value) },
-                };
+                    {"processor-count", () => target.ProcessorNumber = (int?)ParseUint23(value)},
+                    {"runtime-library", () => target.RuntimeLibrary = ParseEnum<RuntimeLibraryType>(value, "runtime library")},
+                    {"runtime-type-info", () => target.RuntimeTypeInfo = ParseBool(value)},
+                    {"smaller-type-check", () => target.SmallerTypeCheck = ParseBool(value)},
+                    {"string-pooling", () => target.StringPooling = ParseBool(value)},
+                    {"struct-member-alignment", () => target.StructMemberAlignment = (int?) ParseUint23(value)},
+                    {"warnings-as-error", () => ParseWarningsAsError(target, value)},
+                    {"treat-wchart-as-buildin-type", () => target.TreatWCharTAsBuiltInType = ParseBool(value)},
+                    {"undefine-preprocessor-definitions", () => ParseUndefinePreprocessorDefinitions(target, value)},
+                    {"warning-level", () => target.WarningLevel = ParseWarningLevel(value)},
+                    {"whole-program-optimization", () => target.WholeProgramOptimization = ParseBool(value)}
+                    };
 
             foreach (var pair in mapping)
                 if (NameIs(name, pair.Key))
                     pair.Value();
+        }
+
+        private CppWarningLevel ParseWarningLevel(YamlNode value)
+        {
+            var sval = ParseString(value).ToLowerInvariant();
+            switch (sval)
+            {
+                case "off":
+                    return CppWarningLevel.Off;
+                case "1":
+                    return CppWarningLevel.Level1;
+                case "2":
+                    return CppWarningLevel.Level2;
+                case "3":
+                    return CppWarningLevel.Level3;
+                case "4":
+                    return CppWarningLevel.Level4;
+                case "all":
+                    return CppWarningLevel.All;
+                default:
+                    throw new InvalidSpecificationException(
+                        String.Format("Invalid warning level: {0}. Should be 'off', '1', '2', '3', '4' or 'all'.", sval));
+            }
+        }
+
+        private void ParseUndefinePreprocessorDefinitions(VCppProjectCompilerParameters target, YamlNode value)
+        {
+            var seq = value as YamlSequenceNode;
+            if (seq != null)
+                target.UndefinePreprocessorDefinitions = ParseStringArray(value);
+            else
+                target.UndefineAllPreprocessorDefinitions = ParseBool(value);
+        }
+
+        private void ParseWarningsAsError(VCppProjectCompilerParameters target, YamlNode value)
+        {
+            var seq = value as YamlSequenceNode;
+            if (seq != null)
+                target.SpecificWarningsAsError = ParseWarnings(value);
+            else
+                target.AllWarningsAsError = ParseBool(value);
+        }
+
+
+        private int[] ParseWarnings(YamlNode value)
+        {
+            var seq = value as YamlSequenceNode;
+            if (seq != null)
+                return seq.Children.OfType<YamlScalarNode>().Select(childValue => Int32.Parse(childValue.Value)).ToArray();
+            else
+                return new int[0];
+        }
+
+        private CLanguage ParseCLanguage(YamlNode value)
+        {
+            var sval = ParseString(value).ToLowerInvariant();
+            switch (sval)
+            {
+                case "default":
+                    return CLanguage.Default;
+                case "c":
+                    return CLanguage.CompileAsC;
+                case "c++":
+                    return CLanguage.CompileAsCpp;
+                default:
+                    throw new InvalidSpecificationException(String.Format("Invalid language specification: {0}. Should be 'default', 'c' or 'c++'", sval));
+            }
         }
 
         private T ParseEnum<T>(YamlNode value, string description) where T : struct
