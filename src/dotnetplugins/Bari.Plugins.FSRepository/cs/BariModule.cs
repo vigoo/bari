29e2e06e79b6da3675119706d26afa25822b80fc
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-09-18 19:25:21 +0200

    Version 0.6

---
+++ src/dotnetplugins/Bari.Plugins.FSRepository/cs/BariModule.cs
@@ -0,0 +1,23 @@
+﻿using Ninject.Modules;
+
+namespace Bari.Plugins.FSRepository
+{
+    /// <summary>
+    /// The module definition of this bari plugin
+    /// </summary>
+    public class BariModule : NinjectModule
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(BariModule));
+
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            log.Info("FSRepository plugin loaded");
+
+//            Bind<IReferenceBuilder>().To<NugetReferenceBuilder>().Named("nuget");
+//            Bind<INuGet>().To<NuGet>();
+        }
+    }
+}
