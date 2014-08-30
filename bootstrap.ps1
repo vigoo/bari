f4dfd9b585534d1e3e55fd2b414348043ff7ba04
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2014-08-30 22:04:00 +0200

    Updated bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,14 +1,29 @@
+Write-Output "Installing Chocolatey"
+
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Output "Installing wget"
+
+cinst Wget
+
+Write-Output "Installing 7zip"
+
+cinst 7zip.commandline
+
+Write-Output "Creating _bootstrap directory"
+
 $root = Get-Location
 $bootstrap = Join-Path $root "_bootstrap"
-New-Item -name _bootstrap -type directory
+New-Item -name _bootstrap -type directory -force
 
-$webclient = New-Object System.Net.WebClient
-$url = "https://github.com/vigoo/bari/releases/download/0.8/bari-0.8.zip"
-$file = Join-Path $bootstrap "bari.zip"
-$webclient.DownloadFile($url,$file)
+Set-Alias zip "7za.exe"
 
-$shell_app=New-Object -com shell.application
-$zip_file = $shell_app.namespace($file)
-$destination = $shell_app.namespace($bootstrap)
-$destination.Copyhere($zip_file.items())
+$barizip = Join-Path $bootstrap "bari.zip"
 
+Write-Output "Downloading bari"
+
+wget --no-check-certificate "https://github.com/vigoo/bari/releases/download/0.8/bari-0.8.zip" -O $barizip
+
+Write-Output "Unpacking bari"
+
+zip x $barizip -o_bootstrap
