372057db93a5b18c93b76e6a33bcc9028d27d4c6
Psychobilly87 <tzoli14@freemail.hu>
2014-11-05 12:07:46 +0100

    Remove unised using

--- src/core/bari/cs/Program.cs
+++ src/core/bari/cs/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using Bari.Console.UI;
 using Bari.Core;
 using Bari.Core.Build;
