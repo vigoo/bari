2eb89e5cba115e6d05feba52b1adcdb5359ce95c
Zoltan Tibor <tzoli14@freemail.hu>
2019-03-27 10:21:47 +0100

    New TargetFramework support

--- src/dotnetplugins/Bari.Plugins.Csharp/cs/Model/CsharpProjectParameters.cs
+++ src/dotnetplugins/Bari.Plugins.Csharp/cs/Model/CsharpProjectParameters.cs
@@ -428,6 +428,18 @@ namespace Bari.Plugins.Csharp.Model
                     return "v4.5";
                 case FrameworkVersion.v451:
                     return "v4.5.1";
+                case FrameworkVersion.v452:
+                    return "v4.5.2";
+                case FrameworkVersion.v46:
+                    return "v4.6";
+                case FrameworkVersion.v461:
+                    return "v4.6.1";
+                case FrameworkVersion.v462:
+                    return "v4.6.2";
+                case FrameworkVersion.v47:
+                    return "v4.7";
+                case FrameworkVersion.v471:
+                    return "v4.7.1";
                 default:
                     throw new ArgumentOutOfRangeException("targetFrameworkVersion");
             }
