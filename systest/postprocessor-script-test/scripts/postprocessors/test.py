eb3b4bf8556cb22255840ecb948417d6b4c3268b
gavrail <gavrail@users.noreply.github.com>
2021-01-03 15:29:19 -0500

    Remove line ending in postprocessor-script-test as in systests

--- systest/postprocessor-script-test/scripts/postprocessors/test.py
+++ systest/postprocessor-script-test/scripts/postprocessors/test.py
@@ -4,7 +4,7 @@ print 'Target dir:' + targetDir
 
 genExe = get_tool("fsrepo://MessageGenerator/*.*", "MessageGenerator.exe")
 
-cmd = genExe + ' "' + targetDir+os.path.sep+'generated.txt" "Hello_world\n"'
+cmd = genExe + ' "' + targetDir+os.path.sep+'generated.txt" "Hello_world"'
 
 if is_mono:
     cmd  = 'mono ' + cmd
